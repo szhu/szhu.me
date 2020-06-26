f2bc5b7b6b2fbfaa2cd5bf988a411c9c37e2eebe
Sean Zhu <interestinglythere@gmail.com>
2020-06-25 18:05:15 -0700

    Add files for new version

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable filenames/match-exported */
+import React from "react";
+
+const Page: React.FC = () => {
+  return (
+    <>
+      Hello!
+      <br />
+      https://github.com/szhu
+      <br />
+      https://www.linkedin.com/in/terestinglythere
+    </>
+  );
+};
+
+export default Page;
